9220fb14fab0e0b8d7b7f3068cf736063af91077
Chris Parton <chrisparton1991@gmail.com>
2018-03-20 13:11:24 +1100

    Split first lecture into two parts

--- 01_BombDefusalGame/02_OurFirstProgram/BombDefusalGame/BombDefusalGame.ino
+++ 01_BombDefusalGame/02_OurFirstProgram/BombDefusalGame/BombDefusalGame.ino
@@ -1,30 +1,14 @@
 #include "FastLED.h"
 
 #define NUM_LEDS 12
-#define DATA_PIN 10
 
 CRGB leds[NUM_LEDS];
-int playerPos = 0;
 
 void setup() {
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  startGame();
+  FastLED.addLeds<WS2812B, 10, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-
-  movePlayer();
-  leds[playerPos] = CRGB::Blue;
+  leds[0] = CRGB::Blue;
   FastLED.show();
-
-  delay(100);
-}
-
-void startGame() {
-  playerPos = 0;
-}
-
-void movePlayer() {
-  playerPos = (playerPos + 1) % NUM_LEDS;
 }
--- 01_BombDefusalGame/03_MovingThePlayer/BombDefusalGame/BombDefusalGame.ino
+++ 01_BombDefusalGame/03_MovingThePlayer/BombDefusalGame/BombDefusalGame.ino
@@ -1,28 +1,23 @@
 #include "FastLED.h"
 
 #define NUM_LEDS 12
-#define DATA_PIN 10
 
 CRGB leds[NUM_LEDS];
 int playerPos = 0;
 
 void setup() {
-  delay(3000);
-
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(160);
+  FastLED.addLeds<WS2812B, 10, GRB>(leds, NUM_LEDS);
   startGame();
 }
 
 void loop() {
   fill_solid(leds, NUM_LEDS, CRGB::Black);
 
-  EVERY_N_MILLIS(100) {
-    movePlayer();
-  }
-
+  movePlayer();
   leds[playerPos] = CRGB::Blue;
   FastLED.show();
+
+  delay(100);
 }
 
 void startGame() {
